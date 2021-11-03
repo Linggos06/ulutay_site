6758d0305876459d27ed90efc655afa3f419e1e7
Linggos06 <julia.snk@hotmail.com>
2021-11-03 17:41:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,23 +13,23 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/contacts', (req, res) => {
+app.get('/ulutay_site/contacts', (req, res) => {
     res.sendFile(__dirname + '/pages/contacts.html');
 }); 
 
-app.get('/about', (req, res) => {
+app.get('/ulutay_site/about', (req, res) => {
     res.sendFile(__dirname + '/pages/about.html');
 });
 
-app.get('/services', (req, res) => {
+app.get('/ulutay_site/services', (req, res) => {
     res.sendFile(__dirname + '/pages/services.html');
 });
 
-app.get('/references', (req, res) => {
+app.get('/ulutay_site/references', (req, res) => {
     res.sendFile(__dirname + '/pages/references.html');
 });
 
-app.get('/info', (req, res) => {
+app.get('/ulutay_site/info', (req, res) => {
     res.sendFile(__dirname + '/pages/info.html');
 });
 
