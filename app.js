781f93c1b2cc63cc1caea04d6051acd731ec0482
Linggos06 <julia.snk@hotmail.com>
2021-11-03 16:17:12 +0300

    some changes in structure of the project

--- app.js
+++ app.js
@@ -10,27 +10,27 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/pages/index.html');
 });
 
 app.get('/contacts', (req, res) => {
-    res.sendFile(__dirname + '/views/contacts.html');
+    res.sendFile(__dirname + '/pages/contacts.html');
 }); 
 
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/views/about.html');
+    res.sendFile(__dirname + '/pages/about.html');
 });
 
 app.get('/services', (req, res) => {
-    res.sendFile(__dirname + '/views/services.html');
+    res.sendFile(__dirname + '/pages/services.html');
 });
 
 app.get('/references', (req, res) => {
-    res.sendFile(__dirname + '/views/references.html');
+    res.sendFile(__dirname + '/pages/references.html');
 });
 
 app.get('/info', (req, res) => {
-    res.sendFile(__dirname + '/views/info.html');
+    res.sendFile(__dirname + '/pages/info.html');
 });
 
 
