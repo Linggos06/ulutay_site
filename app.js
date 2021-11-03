91d6aab9eb330344262430ca07df70a3becee706
Linggos06 <julia.snk@hotmail.com>
2021-11-03 17:00:36 +0300

    fixed links

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contacts', (req, res) => {
